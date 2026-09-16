a09ed7ac05631e74795888f105ac3236ff352c5d
茶白 <1332709015@qq.com>
2026-09-16 17:41:58 +0800

    拦截器配置

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.hmdp.utils;
+
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //1.获取session
+        User user = (User)request.getSession().getAttribute("user");
+        //2.判断用户是否登录
+        if (user == null) {
+            //3.未登录则返回登录页面
+            response.setStatus(401);
+            return false;
+        }
+        //4.用户存在，保存用户信息
+        UserHolder.saveUser(user);
+        //5. 放行
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 移除用户
+        UserHolder.removeUser();
+    }
+
+}
