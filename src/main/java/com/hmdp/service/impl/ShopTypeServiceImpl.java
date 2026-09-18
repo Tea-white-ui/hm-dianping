b5d67cc1dcb04b72586d30cd4193076cd2896ea3
茶白 <1332709015@qq.com>
2026-09-18 18:00:22 +0800

    店铺分类信息缓存功能

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_LIST_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_TTL;
 
 /**
  * <p>
@@ -16,5 +29,27 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryTypeList() {
+        // 1. 从缓存中读取数据
+        String typeListJSON = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_LIST_KEY);
+
+        if (StrUtil.isNotBlank(typeListJSON)) {
+            // 2. 存在，直接返回
+            stringRedisTemplate.expire(CACHE_SHOP_TYPE_LIST_KEY, CACHE_SHOP_TYPE_TTL, TimeUnit.MINUTES);
+            return Result.ok(JSONUtil.toList(typeListJSON,ShopType.class));
+        }
+
+        // 3. 不存在，查询数据库
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+
+        // 4. 存入缓存
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_LIST_KEY, JSONUtil.toJsonStr(shopTypes), CACHE_SHOP_TYPE_TTL, TimeUnit.MINUTES);
+
+        // 5. 返回结果
+        return Result.ok(shopTypes);
+    }
 }
--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -14,6 +14,9 @@ public class RedisConstants {
     public static final String CACHE_SHOP_TYPE_KEY = "cache:shop:type:";
     public static final String CURRENT_KEY = "current:";
 
+    public static final String CACHE_SHOP_TYPE_LIST_KEY = "cache:shop:type:list";
+    public static final Long CACHE_SHOP_TYPE_TTL = 30L;
+
     public static final String LOCK_SHOP_KEY = "lock:shop:";
     public static final Long LOCK_SHOP_TTL = 10L;
 
